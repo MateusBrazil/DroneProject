7dc6edfe90e4fd94aa032f9b9c343af0da8149ed
Bruno <brunosdsj@gmail.com>
2021-10-14 22:27:31 -0300

    Organização de funções e nova função de configuração do range do Giroscópio

--- MPU_6050.cpp
+++ MPU_6050.cpp
@@ -1,60 +1,30 @@
 #include "MPU_6050.h"
 
-
-MPU_6050::MPU_6050(){}
-
+MPU_6050::MPU_6050() {}
 
 int16_t MPU_6050::GetData(uint16_t reg_address, uint8_t num_bytes){
-  int16_t value;
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(MPU_SENSOR_I2C_ADDRESS);
   Wire.write(reg_address);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, num_bytes, true);
-
-  value = Wire.read() << 8 | Wire.read();
+  Wire.requestFrom(MPU_SENSOR_I2C_ADDRESS, num_bytes, true);
+  int16_t value = Wire.read() << 8 | Wire.read();
   return value;
 }
 
 float MPU_6050::AccelCalculator(int16_t raw_accel){
 
-  float no_accel = 0.0;
-
   float rangePerUnit = .000061f;
 
-  no_accel = raw_accel * rangePerUnit * 9.80665;
+  float no_accel = raw_accel * rangePerUnit * 9.80665;
 
   return no_accel;
 
 }
 
-void MPU_6050::begin(){
-  
-  const short int FS_SEL = 0;  
-
-  Serial.println("ESTOU NA BEGIN");
+void MPU_6050::initiate(){
   
   Wire.begin();
-  Wire.beginTransmission(MPU);
-  int8_t mpu_status = Wire.endTransmission();
-
-  //if(mpu_status != 0) return mpu_status;
-
-  //Configuração da escala dos registradores do giroscopio
-  Serial.println("########################################");
-  Serial.println("Configurando registradores do giroscopio");
-  Wire.beginTransmission(MPU);
-  Wire.write(GYRO_CONFIG);
-  Wire.write(GYRO_FS_SEL_CFG);
-  Wire.endTransmission();
-  Serial.println("##########Configuração finalizada#######");
-  Serial.println("########################################");        
-  //Fim da configuração dos registradores do giroscopio
-
-  Serial.println("#######################Configurações#######################");
-  Serial.println("## Registrador FS_SEL = " + String(FS_SEL));
-  Serial.println("###########################################################");
-
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(MPU_SENSOR_I2C_ADDRESS);
   Wire.write(POWER_MANAGEMENT);
   Wire.write(POWER_MANAGEMENT_CFG);
   Wire.endTransmission(true);
@@ -62,27 +32,30 @@ void MPU_6050::begin(){
 }    
 
    
-   
+void MPU_6050::ConfigGyroRange(int gyro_range){
+
+	Wire.beginTransmission(MPU_SENSOR_I2C_ADDRESS);
+	Wire.write(GYRO_CONFIG);
+	Wire.write(GYRO_FS_SEL_CFG);
+	Wire.endTransmission();
+}
+
 int MPU_6050::GyroX(){
 
-  int16_t result = GetData(GYRO_XOUT_H, 2);
-  double radians = double(result/32.8);  
-  return radians;
+	int16_t result = GetData(GYRO_XOUT_H, 2);
+
 }
 
 int MPU_6050::GyroY(){
- 
-  int16_t result = GetData(GYRO_YOUT_H, 2);
-  double radians = double(result/32.8);
-  
-  return radians;
+	
+	int16_t result = GetData(GYRO_YOUT_H, 2);
+
 }   
 
 int MPU_6050::GyroZ(){        
 
   int16_t result = GetData(GYRO_ZOUT_H, 2);
-  double radians = double(result/32.8);        
-  return radians;
+
 }
 double MPU_6050::Temp(){
 
@@ -90,9 +63,8 @@ double MPU_6050::Temp(){
   int16_t value = GetData(0x41, 2);
   result = double(value / 340.00) + 36.53;
   return result;
+
 }
-
-
 
 float MPU_6050::AccelX(){
 
--- MPU_6050.h
+++ MPU_6050.h
@@ -5,44 +5,43 @@
 #include <stdint.h>
 #include <Wire.h>
 
-//Registers
-#define MPU_SENSOR_I2C_ADDRESS 0x68     //Address of MPU6050 in I2C bus
-#define GYRO_CONFIG 0x1B                //The configuration register of Gyroscope
-#define ACCEL_CONFIG 0x1C               //The configuration register of Accelerometer 
-#define ACCEL_XOUT_H 0x3B               //Register that contains the most valuable bits of the X axis of accelerometer --> this returns 8 bits       
-#define ACCEL_XOUT_L 0x3C               //Register that contains the least valuable bits of the X axis of accelerometer --> this returns 8 bits
-#define ACCEL_YOUT_H 0x3D               //
-#define ACCEL_YOUT_L 0x3E               //  
-#define ACCEL_ZOUT_H 0x3F               //  
-#define ACCEL_ZOUT_L 0x40               //  
-#define TEMP_OUT_H 0x41                 //
-#define TEMP_OUT_L 0x42                 //
-#define GYRO_XOUT_H 0x43                //
-#define GYRO_XOUT_L 0x44                //
-#define GYRO_YOUT_H 0x45                //                    
-#define GYRO_YOUT_L 0x46                //
-#define GYRO_ZOUT_H 0x47                //
-#define GYRO_ZOUT_L 0x48                //
-#define POWER_MANAGEMENT 0x6B           //
-//end of registers
+// BEGIN of registers
+#define MPU_SENSOR_I2C_ADDRESS 0x68      //Address of MPU6050 in I2C bus.
+#define GYRO_CONFIG 0x1B                 // The configuration register of Gyroscope.
+#define ACCEL_CONFIG 0x1C                // The configuration register of Accelerometer.
+#define ACCEL_XOUT_H 0x3B                // Register that contains the most valuable bits of the X axis of accelerometer --> this returns 8 bits.
+#define ACCEL_XOUT_L 0x3C                // Register that contains the least valuable bits of the X axis of accelerometer --> this returns 8 bits.
+#define ACCEL_YOUT_H 0x3D                // Register that contains the most valuable bits of the Y axis of accelerometer --> this returns 8 bits.
+#define ACCEL_YOUT_L 0x3E                // Register that contains the least valuable bits of the Y axis of accelerometer --> this returns 8 bits.
+#define ACCEL_ZOUT_H 0x3F                // Register that contains the most valuable bits of the Z axis of accelerometer --> this returns 8 bits.
+#define ACCEL_ZOUT_L 0x40                // Register that contains the least valuable bits of the Z axis of accelerometer --> this returns 8 bits.
+#define TEMP_OUT_H 0x41                  // Register that contains the most valuable bits of the termometer --> this returns 8 bits.
+#define TEMP_OUT_L 0x42                  // Register that contains the least valuable bits of the termometer --> this returns 8 bits.
+#define GYRO_XOUT_H 0x43                 // Register that contains the most valuable bits of the X axis of Gyroscope --> this returns 8 bits.
+#define GYRO_XOUT_L 0x44                 // Register that contains the least valuable bits of the X axis of Gyroscope --> this returns 8 bits.
+#define GYRO_YOUT_H 0x45                 // Register that contains the most valuable bits of the Y axis of Gyroscope --> this returns 8 bits.
+#define GYRO_YOUT_L 0x46                 // Register that contains the least valuable bits of the Y axis of Gyroscope --> this returns 8 bits.
+#define GYRO_ZOUT_H 0x47                 // Register that contains the most valuable bits of the Z axis of Gyroscope --> this returns 8 bits.
+#define GYRO_ZOUT_L 0x48                 // Register that contains the least valuable bits of the Z axis of Gyroscope --> this returns 8 bits.
+#define POWER_MANAGEMENT 0x6B            // Read/Write register that configurate power management of MPU_6050.
+// END of registers
 
-//Register Configuration
-//The configuration values are on the datasheet of registers
+// Register Configuration
+// The configuration values are on the datasheet of registers.
 #define POWER_MANAGEMENT_CFG 0x00          
 #define ACCEL_FS_SEL_CFG 0x00                 
 #define GYRO_FS_SEL_CFG 0x10  
 //end of register configuration                
 
 /*!
- * @brief Main MPU6050 class
+ * @brief MPU_6050 Main Class
  */
 class MPU_6050
 {
 
   private:
-    const uint16_t MPU = MPU_SENSOR_I2C_ADDRESS;
-    int mpu_status = 404;
-    bool control = true;
+
+    MPU_6050();
 
     /*!
      * @brief Get data from registers over I2C from the MPU6050
@@ -72,13 +71,11 @@ class MPU_6050
   
   
   public:
-    
-    MPU_6050();    
 
      /*!
      * @brief Starts the I2C communication with the MPU6050
      */
-    void begin();    
+    void initiate();    
 
     /*!
      * @brief Gets the X value of Gyroscope
@@ -121,6 +118,16 @@ class MPU_6050
      * @return Returns X value in m/s²
      */
     float AccelZ();
+
+    /*!
+     * @brief Configure the Gyroscope range. 
+     * 0 = 250 rads/s;
+     * 1 = 500 rads/s;
+     * 2 = 1000 rads/s;
+     * 3 = 2000 rads/s;
+     * @param gyro_range The Gyroscope sensitivity in a scale 0-3
+     */
+    void ConfigGyroRange(int gyro_range);
 
 };
 
